1833ee662056879b481a7e5bf4a1ce0f3b6ed795
tohanay <tohanay@gmail.com>
2022-07-23 16:29:21 +0300

    add create/edit, basic navigation

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\Pin;
 use App\Repository\PinRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,7 +13,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class PinsController extends AbstractController
 {
     /**
-     * @Route("/", name="app_home")
+     * @Route("/", name="app_home", methods="GET")
      */
     public function index(PinRepository $pinRepository): Response
     {
@@ -21,11 +23,68 @@ class PinsController extends AbstractController
     }
 
     /**
-     * @Route("/pins/{id<[0-9]+>}", name="app_pins_show")
+     * @Route("/pins/{id<[0-9]+>}", name="app_pins_show", methods="GET")
      */
     public function show(Pin $pin): Response
     {
 
-        return $this->render('pins/show.html.twig', compact('pin'));
+        return $this->render('pins/show.html.twig', compact('pin')); //grâce au bundle sensio/framework-extra-bundle sinon on aura utilisé un repository avec la méthode find($id)
+    }
+
+     /**
+     * @Route("/pins/edit/{id<[0-9]+>}", name="app_pins_edit", methods={"GET", "POST"})
+     */
+    public function edit(Request $request, EntityManagerInterface $em, Pin $pin): Response
+    {
+        $form = $this->createFormBuilder($pin)
+        ->add('title', null, ['attr'=> ['autofocus' => true]])
+        ->add('description', null)
+        ->getForm()
+        ;
+     
+        $form->handleRequest($request); //récupérer les données du formulaire via $request
+ 
+        if ($form->isSubmitted() && $form->isValid()) {
+         
+         $em->persist($pin); // on n'a plus besoin de faire un $form->getData() car l'objet passé au createFormBuilder est récupéré bel et bien et a été mis à jour par les données entrées au niveau du formulaire
+         $em->flush();
+ 
+         return $this->redirectToRoute('app_pins_show',['id' => $pin->getId()]);
+ 
+        }
+          
+        return $this->render('pins/edit.html.twig', [
+            'pin' => $pin,
+            'monFormulaire' => $form->createView()
+           ]);
+    }
+
+    /**
+     * @Route("/pins/create", name="app_pins_create", methods={"GET", "POST"})
+     */
+    public function create(Request $request, EntityManagerInterface $em) :Response
+    {
+        $pin = new Pin;
+        
+        $form = $this->createFormBuilder($pin)
+        ->add('title', null, ['attr'=> ['autofocus' => true]])
+        ->add('description', null)
+        ->getForm()
+        ;
+     
+        $form->handleRequest($request); //récupérer les données du formulaire via $request
+ 
+        if ($form->isSubmitted() && $form->isValid()) {
+         
+         $em->persist($pin); // on n'a plus besoin de faire un $form->getData() car l'objet passé au createFormBuilder est récupéré bel et bien et a été mis à jour par les données entrées au niveau du formulaire
+         $em->flush();
+ 
+         return $this->redirectToRoute('app_pins_show',['id' => $pin->getId()]);
+ 
+        }
+ 
+        return $this->render('pins/create.html.twig', [
+         'monFormulaire' => $form->createView()
+        ]);
     }
 }
