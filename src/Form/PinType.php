7d12690b0101d749e0f4a985f6e378d035512b37
tohanay <tohanay@gmail.com>
2022-07-27 20:03:45 +0300

    add liip imagine bundle and add imagine_pattern attribut in PinType

--- src/Form/PinType.php
+++ src/Form/PinType.php
@@ -5,13 +5,23 @@ namespace App\Form;
 use App\Entity\Pin;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+
 
 class PinType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('imageFile', VichImageType::class, [
+                'label'=>'Image (JPG ou PNG file)',
+                'required' => false,
+                'allow_delete' => true,
+                'download_uri' => false,
+                'imagine_pattern' => 'squared_thumbnail_antonony',
+               
+            ])
             ->add('title')
             ->add('description')
         ;
